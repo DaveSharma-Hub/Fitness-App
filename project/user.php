be70d1e93147526e9af41a9f996099ab6da566e9
DaveSharma-Hub <davesharma12345@outlook.com>
2022-03-20 16:54:39 -0600

    Added Menu Stuff

--- project/user.php
+++ project/user.php
@@ -1,8 +1,10 @@
 <?php
+    session_start();
 
     $username = $_POST["uname"];
     $password = $_POST["psw"];
 
     echo $username." ".$password;
-    
+    $_SESSION['login'] = $username;
+    header('Location: userMenu.php');
 ?>
